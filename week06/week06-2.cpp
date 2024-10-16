1edac60046150b8adf674102d520a26e606016cb
Chhokmah00 <5m4wu6@gmail.com>
2024-10-16 10:43:50 +0800

    week06

---
+++ week06/week06-2.cpp
@@ -0,0 +1,18 @@
+/// CPE 10107 what the median
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    int now;
+    while(cin>>now){
+        a.push_back(now);
+        sort(a.begin(),a.end());
+        int N=a.size();
+        if(N%2==1) cout<<a[N/2]<<"\n";
+        else cout<<(a[N/2-1]+a[N/2])/2<<"\n";
+    }
+}
