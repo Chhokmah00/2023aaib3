a57c55ca8b1985b1b702a534142ad0862ed36d9b
Chhokmah00 <5m4wu6@gmail.com>
2024-12-04 10:48:45 +0800

    week13

---
+++ week13/week13-3.cpp
@@ -0,0 +1,16 @@
+int main() {
+    int a,b;
+    vector<int> A,B;
+    while(cin>>a>>b){
+        A.push_back(a); //把數字a放入陣列A
+        B.push_back(b); //把數字b放入陣列B
+    } //把A、B從小到大排好
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    int ans=0;
+    for(int i=0;i<A.size();i++){
+        //cout<<A[i]<<" ";
+        ans+=abs(A[i]-B[i]);
+    }
+    cout<<"加起來的答案是"<<ans;
+}
